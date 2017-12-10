6008c3cd6a5aff244f0b701a927749af7c66268a
Ingrid Medeiros <i.smedeiros@hotmail.com>
2017-12-10 11:32:55 -0200

    #8 - Configurando Ambiente de Desenvolvimento

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,6 @@
 const { sayHelloTo } = require('./hello.js');
 
 const span = document.createElement('span');
-span.innerText = sayHelloTo('Javascript Developer');
+span.innerText = sayHelloTo('Ingrid Medeiros');
 
 document.body.appendChild(span);
