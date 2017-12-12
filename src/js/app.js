272aecadb8f77339220ac2d0ed3f187162dba475
Ingrid Medeiros <i.smedeiros@hotmail.com>
2017-12-11 22:15:06 -0200

    #11 - Atualizando Browser sem Refresh com HMR

--- src/js/app.js
+++ src/js/app.js
@@ -4,3 +4,8 @@ const span = document.createElement('span');
 span.innerText = sayHelloTo('Ingrid!');
 
 document.body.appendChild(span);
+
+if (module.hot) {
+	module.hot.accept();
+	module.hot.dispose(() => span.parentNode.removeChild(span));
+}
