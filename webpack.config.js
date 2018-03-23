ebd9ce60de699d65540e5875fc41a00c0a4edd95
Ingrid Medeiros <i.smedeiros@hotmail.com>
2018-03-23 20:38:41 -0300

    Commit com bug -

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 var webpack = require('webpack');
 const path = require('path');
 
+const resolvePath = (pathToResolve = '') => path.resolve(__dirname, pathToResolve)
+const isProductionEnvironment = process.env.NODE_ENV === "production"
+
 module.exports = {
   entry: './src/ts/app/app.ts',
   output: {
@@ -12,12 +15,12 @@ module.exports = {
   	loaders: [
   		{
             test: /\.ts$/,
-            include: [path.resolve(__dirname, './src/ts')],
+            include: [resolvePath('src/ts')],
             loader: 'ts-loader'
         },
         {
             test: /\.s(a|c)ss$/, //.sass / .scss
-            include: [path.resolve(__dirname, './src/styles')],
+            include: [resolvePath('./src/styles')],
             loader: 'style-loader!css-loader!sass-loader'
         }
   	]
@@ -26,16 +29,15 @@ module.exports = {
   resolve: {
     extensions: ['.webpack.js', '.web.js', '.js', '.ts', '.scss'],
   	alias: {
-  		styles: path.resolve(__dirname, 'src/styles'),
-  		helpers: path.resolve(__dirname, 'src/ts/helpers')
+  		styles: resolvePath('src/styles'),
+  		helpers: resolvePath('src/ts/helpers')
   	}
   },
-  plugins: [
+  plugins: isProductionEnvironment ?  [
     new webpack.optimize.UglifyJsPlugin({
         compress: {
-            warnings: false,
-            drop_console: false,
+            warnings: false
         }
-    })  
-  ]
+    }) 
+  ] : []
 }
